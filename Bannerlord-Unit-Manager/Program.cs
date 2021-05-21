e6a23cb4e3500a49464d54fa25c3873cb357f75f
Gurditt Rai <gurdittrai@hotmail.com>
2021-05-21 18:54:08 -0400

    Add project files.

---
+++ Bannerlord-Unit-Manager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bannerlord_Unit_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
